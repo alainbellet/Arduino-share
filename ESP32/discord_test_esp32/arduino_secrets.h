2d283b378329ca993e24a0d33c0f07337f48c904
Alain Bellet <alainbellet@users.noreply.github.com>
2021-01-26 14:27:53 +0100

    Update arduino_secrets.h

--- ESP32/discord_test_esp32/arduino_secrets.h
+++ ESP32/discord_test_esp32/arduino_secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
+#define SECRET_SSID "sstudios"
+#define SECRET_PASS "quickbrownhunz"
 //Copy the webhook url here:
-#define SECRET_WEBHOOK "https://discord.com/api/webhooks/..."
+#define SECRET_WEBHOOK "https://discord.com/api/webhooks/803584053513158687/L1WZV7hBGzRMAyqy3NsWEAJlftzvvQ1Au3BtOBKyLDQ0Dovv4gTfug-kd0wR5JGZM2Tz"
 #define SECRET_TTS "true"
