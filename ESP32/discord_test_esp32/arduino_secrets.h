af619ba532f1df58f3b42c1ba452f2246f105359
Alain Bellet <alainbellet@users.noreply.github.com>
2021-01-26 13:59:52 +0100

    remove credentials

--- ESP32/discord_test_esp32/arduino_secrets.h
+++ ESP32/discord_test_esp32/arduino_secrets.h
@@ -1,7 +1,5 @@
-#define SECRET_SSID "sstudios"
-#define SECRET_PASS "quickbrownhunz"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
 //Copy the webhook url here:
-#define SECRET_WEBHOOK "https://discord.com/api/webhooks/803584053513158687/L1WZV7hBGzRMAyqy3NsWEAJlftzvvQ1Au3BtOBKyLDQ0Dovv4gTfug-kd0wR5JGZM2Tz"
-//
-// GAEL -> https://discord.com/api/webhooks/803604460823248966/yqSO9ioT_j_EirSvm_aZF2OMBE36Hz0FeTsZg3taZH6iLp6ditI0jzj8KxfQEcHBZ4pj
+#define SECRET_WEBHOOK "https://discord.com/api/webhooks/......"
 #define SECRET_TTS "true"
