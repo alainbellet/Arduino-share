e0136eb092a72f0eea8e92099b5c2e26a098354d
Alain Bellet <alainbellet@users.noreply.github.com>
2021-02-11 18:04:21 +0100

    update for ecal use

--- ESP32/discord_test_esp32/discord_test_esp32.ino
+++ ESP32/discord_test_esp32/discord_test_esp32.ino
@@ -1,16 +1,29 @@
-#include "arduino_secrets.h"
 /*
   Discord WebHook Example for ESP32
+  Based on: https://www.instructables.com/Send-a-Message-on-Discord-Using-Esp32-Arduino-MKR1/
+  
+  Adapted and commented by Alain Bellet / ECAL
+
+  In order to use this you will need:
+  - an esp32 based board
+  - a discord webhook 
+  - an SSL certificate to allow https communication 
+  - a wifi connection (2.4 GHZ)
+
+  -> Enter your wifi and discord credentials in the "arduino_secrets.h" Tab
+  
 */
+
+#include "arduino_secrets.h"
 #include "discord.h"
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   connectWIFI();
-  sendDiscord("je dis ça, je dis rien!");
+  sendDiscord("esp32 is online");
 }
 
 void loop() {
-  delay(20 * 1000);
-  sendDiscord("je dis la meme chose toutes les 20 secondes");
+  delay(30 * 1000);
+  sendDiscord("i say that every 30 seconds");
 }
