00d82c050639173536ae99248c85165edc4c96d6
Alain Bellet <alainbellet@users.noreply.github.com>
2022-02-21 17:34:22 +0100

    push

--- RP2040/test_haptic_feedback_DRV2605/test_haptic_feedback_DRV2605.ino
+++ RP2040/test_haptic_feedback_DRV2605/test_haptic_feedback_DRV2605.ino
@@ -2,7 +2,6 @@
 #include "Adafruit_DRV2605.h"
 
 Adafruit_DRV2605 drv;
-int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
   Serial.begin(9600);
@@ -14,29 +13,31 @@ void setup() {
   // I2C trigger by sending 'go' command
   // default, internal trigger when sending GO command
   drv.setMode(DRV2605_MODE_INTTRIG);
-  //drv.setMode(1);
-  
+  Serial.println("ERM: (Eccentric Rotating Mass) mode active"); // Default
+
+  Serial.println("which effect? (1-123)");        //Prompt User for input
+  Serial.println("ERM or LRA mode? (ERM type 200; LRA type 300)");        //Prompt User for input
+  //drv.useERM(); //Use ERM (Eccentric Rotating Mass) mode.
+  drv.useLRA(); //Use LRA (Linear Resonance Actuator) mode.
 }
 
-uint8_t effect = 17;
+uint8_t effect = 1;
+
 
 void loop() {
-  sensorValue = analogRead(A0);
-  if (sensorValue % 100 == 1) {
-
-    //Serial.println("which effect?");        //Prompt User for input
-    //while (Serial.available() == 0) {}          // wait for user input
-    //effect = Serial.parseInt();
-
+  int cmd = 0;
+  while (Serial.available() > 0)
+  {
+    cmd = Serial.parseInt();
+    if (cmd != 0) {
+      effect = cmd;
+    }
+    Serial.println("Effect " + String(effect) + " triggered");
     // set the effect to play
     drv.setWaveform(0, effect);  // play effect
     drv.setWaveform(1, 0);       // end waveform
 
     // play the effect!
     drv.go();
-    // wait a bit
-    delay(200);
   }
-
-
 }
