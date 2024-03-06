bc458064bc18c395cae29e139e3da0aa6e1e62f7
Alain Bellet <alainbellet@users.noreply.github.com>
2024-03-06 14:20:13 +0100

    Update your_secrets.h

--- ESP32/OSC-Unreal-example/your_secrets.h
+++ ESP32/OSC-Unreal-example/your_secrets.h
@@ -1,5 +1,5 @@
-#define WIFI_SSID "sstudios"
-#define WIFI_PASS "quickbrownhunz"
+#define WIFI_SSID "---"
+#define WIFI_PASS "---"
 // autre wifi
 //#define WIFI_SSID "xx"
 //#define WIFI_PASS "xx/"
